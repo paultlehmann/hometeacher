24b0f23b9115b43e5e637defe8907bba0b52df02
swellwrench <swellwrench@gmail.com>
2020-08-24 14:42:32 -0500

    Added connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
-// const app = express();
+// Backend server port
 const port = 5000;
 
 express().use(cors());
 express().use(express.json());
+
+// Connection to MongoDB
+const uri = "mongodb+srv://testuser:test@users.kahfi.mongodb.net/users?retryWrites=true&w=majority";
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connection.once("open", function() {
+    console.log("Successfully connected to MongoDB")
+})
+
 
 express().listen(port, function() {
     console.log(`Server is running on port ${port}`);
